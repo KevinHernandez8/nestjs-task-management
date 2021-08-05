e624494fc5735abade673d27d12189cdcc2b0393
Kevin <kevin.hernandez@sundevs.com>
2021-08-05 17:06:49 -0500

    get a task by id

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,6 +11,10 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTaskById(id: string): Task {
+    return this.tasks.find((_task) => _task.id === id);
+  }
+
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
     const newTask: Task = {
