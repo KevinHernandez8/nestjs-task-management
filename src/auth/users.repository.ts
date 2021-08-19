59bf4732cff8dfdd51426746591036f02585b705
Kevin <kevin.hernandez@sundevs.com>
2021-08-18 21:05:12 -0500

    include password encrypting

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,12 +5,18 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(createUserDto: CreateUserDto): Promise<void> {
     const { username, password } = createUserDto;
-    const newUser = await this.create({ username, password });
+
+    // hash password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await this.create({ username, password: hashedPassword });
 
     try {
       await this.save(newUser);
