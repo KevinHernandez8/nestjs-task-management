16884e6cd2e0394c45178a4d16294906a8af6368
Kevin <kevin.hernandez@sundevs.com>
2021-09-23 11:02:38 -0500

    add passport types, implement modules and validate user token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 import { UsersRepository } from './users.repository';
 
 @Module({
@@ -15,7 +16,8 @@ import { UsersRepository } from './users.repository';
       signOptions: { expiresIn: 3600 },
     }),
   ],
-  controllers: [AuthController],
+  controllers: [AuthController, JwtStrategy],
   providers: [AuthService],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
